cc80e6675bc448080aaef7f5491d359ebc4b978e
Softech <softechtecnologia@gmail.com>
2015-03-19 10:33:02 -0700

    Primeiro Commit
    Inclusão de arquivos.

---
+++ Suporte_Softech_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef SUPORTE_SOFTECH_PRIVATE_H
+#define SUPORTE_SOFTECH_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"1.0.0.0"
+#define VER_MAJOR	1
+#define VER_MINOR	0
+#define VER_RELEASE	0
+#define VER_BUILD	0
+#define COMPANY_NAME	"Softech Tecnologia"
+#define FILE_VERSION	""
+#define FILE_DESCRIPTION	"Instalador do suporte rápido da Softech Tecnologia"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Softech Tecnologia"
+#define LEGAL_TRADEMARKS	"Softech Tecnologia"
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	""
+#define PRODUCT_VERSION	""
+
+#endif /*SUPORTE_SOFTECH_PRIVATE_H*/
