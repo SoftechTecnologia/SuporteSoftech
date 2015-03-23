fd5aba52baa3eecf20b380bae6c014a2dee8e937
Softech <softechtecnologia@gmail.com>
2015-03-22 18:04:23 -0700

    "ssfi" (bug) e otimização
    Retirado comando "nul" da importação do arquivo de registro; retirado
    delay de 3s para início do arquivo "ssfi.bat"; inseridas aspas duplas
    englobando os caminhos de exclsão dos arquivos remanescentes em
    "ssfi.bat".

--- main.cpp
+++ main.cpp
@@ -4,13 +4,11 @@ int main(){
     system("xcopy /y stsupport.exe %windir% > nul");
     system("xcopy /y stsupport.ico %windir% > nul");
     system("xcopy /y stsupport.html %windir% > nul");
-    system("reg import softech.reg > nul");
+    system("reg import softech.reg");
     system("del softech.reg stsupport.exe stsupport.ico stsupport.html > nul");
-    system("echo ping 192.0.28.9 -n 1 -w 3000 > %temp%\\ssfi.bat");
+    system("echo del \"%cd%\\Instalador.exe\" >> %temp%\\ssfi.bat");
+    system("echo del \"%cd%\\SuporteSoftech*.zip\" >> %temp%\\ssfi.bat");
     system("echo cls >> %temp%\\ssfi.bat");
-    system("echo del %cd%\\Instalador.exe >> %temp%\\ssfi.bat");
-    system("echo cls >> %temp%\\ssfi.bat");
-    system("echo del %cd%\\SuporteSoftech.zip >> %temp%\\ssfi.bat");
     system("echo exit >> %temp%\\ssfi.bat");
     system("start %temp%\\ssfi.bat");
 }
